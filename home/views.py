c35d96902f202b1185e17379808619d3b8094911
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-04 19:39:31 +0330

    GenericProject: ListView with get_queryset method

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, Optional
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import RedirectView, TemplateView, ListView
@@ -7,6 +8,12 @@ from .models import Student
 
 class HomeView(ListView):
     template_name = 'home/home.html'
-    model = Student
     context_object_name = 'student'
     ordering = '-age'
+    allow_empty = False
+    paginate_by = 1
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return Student.objects.all()
+    
+    
