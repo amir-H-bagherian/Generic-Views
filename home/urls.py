0e3749dbf7a604e87569eab5ac6a4f90dbaf79b6
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-04 18:51:35 +0330

    GenericProject: RedirectView

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, FisrtView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('first/', FisrtView.as_view(), name='first'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
-
+from django.views.generic import RedirectView, TemplateView
 from .models import Student
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["student"] = Student.objects.all()
-        return context
-    
+
+class FisrtView(RedirectView):
+    url = '/'
