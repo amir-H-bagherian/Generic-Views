807d691208d81859400691457cb4d7ab8a391a63
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-04 19:14:39 +0330

    GenericProject: RedirectView with path parameters

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomeView, FisrtView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('first/', FisrtView.as_view(), name='first'),
+    path('a/<str:name>/<int:age>/', HomeView.as_view(), name='home'),
+    path('first/<str:name>/<int:age>/', FisrtView.as_view(), name='first'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict, Optional
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import RedirectView, TemplateView
@@ -6,6 +7,19 @@ from .models import Student
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print('another time')
+        print(kwargs)
+        context["name"] = kwargs['name']
+        context["age"] = kwargs['age']
+        return context
+    
 
 class FisrtView(RedirectView):
-    url = '/'
+    pattern_name = 'home'
+    
+    def get_redirect_url(self, *args: Any, **kwargs: Any) -> str | None:
+        print(kwargs)
+        return super().get_redirect_url(*args, **kwargs)
